6ea23913b25401942774a411fc6a452d724fb965
Robin Winslow <robin@robinwinslow.co.uk>
2019-09-12 22:30:48 +0100

    Restructure tests and test basket.subtotal
    Now seems like a reasonable time to start testing against the actual
    baskets mentioned in the assignment.
    
    I don't think we any longer need an actual test for "add_product", as
    it's so central to the whole operation, we can simply use it in our
    setUp, which will obvious fail if that method fails.

--- shopping_basket/shopping_basket_tests/test_basket.py
+++ shopping_basket/shopping_basket_tests/test_basket.py
@@ -1,6 +1,6 @@
 import unittest
+import catalog
 from basket import Basket
-from catalog import products
 
 
 class TestBasket(unittest.TestCase):
@@ -9,9 +9,30 @@ class TestBasket(unittest.TestCase):
     """
 
     def setUp(self):
-        self.basket = Basket(product_catalog=products)
+        """
+        Create the two baskets described in assignment.py
+        """
 
-    def test_add_product(self):
-        self.basket.add_product("beans")
-        self.basket.add_product("shampoo-l")
-        self.assertEqual(len(self.basket.products), 2)
+        self.basket_1 = Basket(product_catalog=catalog.products)
+
+        self.basket_1.add_product("beans")
+        self.basket_1.add_product("beans")
+        self.basket_1.add_product("beans")
+        self.basket_1.add_product("beans")
+        self.basket_1.add_product("biscuits")
+
+        self.basket_2 = Basket(product_catalog=catalog.products)
+
+        self.basket_2.add_product("beans")
+        self.basket_2.add_product("beans")
+        self.basket_2.add_product("biscuits")
+        self.basket_2.add_product("sardines")
+        self.basket_2.add_product("sardines")
+
+    def test_subtotal(self):
+        """
+        Check subtotal() returns the expected sum of all products' prices
+        """
+
+        self.assertEqual(self.basket_1.subtotal(), 5.16)
+        self.assertEqual(self.basket_2.subtotal(), 6.96)
