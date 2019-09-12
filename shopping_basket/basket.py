ca92bcd8447640558ec2c686d57d4b7c1be43d93
Robin Winslow <robin@robinwinslow.co.uk>
2019-09-12 20:53:42 +0100

    Create bare Basket class

---
+++ shopping_basket/basket.py
@@ -0,0 +1,6 @@
+class Basket():
+    """
+    A collection of products that a customer intends to purchase,
+    with methods allowing you to calculate the subtotal and total
+    price of all products, potentially with offers applied.
+    """
