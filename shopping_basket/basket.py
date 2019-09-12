3f20b64a87f13793b47a3cf5f5096d7e13811784
Robin Winslow <robin@robinwinslow.co.uk>
2019-09-12 22:54:06 +0100

    Implement Basket.subtotal(). Also add docstrings

--- shopping_basket/basket.py
+++ shopping_basket/basket.py
@@ -5,10 +5,29 @@ class Basket():
     price of all products, potentially with offers applied.
     """
 
-    products = []
-
     def __init__(self, product_catalog):
         self.product_catalog = product_catalog
+        self.products = []
 
     def add_product(self, sku):
+        """
+        Add a product sku to the basket.
+
+        We're not adding any more information at this stage, as we should
+        only look up the product in the catalog at the last minute so we
+        get the most up-to-date price for each item.
+        """
+
         self.products.append(sku)
+
+    def subtotal(self):
+        """
+        Calculate the subtotal of all products in the basket
+        """
+
+        subtotal = 0
+
+        for sku in self.products:
+            subtotal += self.product_catalog[sku]["price"]
+
+        return round(subtotal, 2)
